65c03f3a6898ec24633520865d35e38b9d2dabd7
fparismusic <filippo.paris@mail.polimi.it>
2025-05-31 11:36:05 +0200

    TextMaker added

--- MonumentSimulator/src/main.cpp
+++ MonumentSimulator/src/main.cpp
@@ -1,7 +1,7 @@
 // This has been adapted from the Vulkan tutorial
 
 #include "modules/Starter.hpp"
-//#include "modules/TextMaker.hpp"
+#include "modules/TextMaker.hpp"
 
 using namespace std;
 using namespace glm;
@@ -9,50 +9,67 @@ using namespace glm;
 // MonumentSimulator: demo subclass of BaseProject
 class MonumentSimulator : public BaseProject {
 protected:
+    /*
+    * SETUP & FUNCTIONS
+    */
 
-    void setWindowParameters() override;
-    void onWindowResize(int w, int h) override;
-    void localInit() override;
-    void pipelinesAndDescriptorSetsInit() override;
-    void populateCommandBuffer(VkCommandBuffer commandBuffer, int i) override;
-    void updateUniformBuffer(uint32_t currentImage) override;
-    void pipelinesAndDescriptorSetsCleanup() override;
-    void localCleanup() override;
 
+
+
+    // Here you set the main application parameters
+	void setWindowParameters()
+	{
+		std::cout << "this is an exemple()\n";
+	}
+
+	// What to do when the window changes size
+	void onWindowResize(int w, int h)
+	{
+		std::cout << "this is an exemple()\n";
+	}
+
+    // Here you load and setup all your Vulkan Models and Texutures.
+	// Here you also create your Descriptor set layouts and load the shaders for the pipelines
+    void localInit()
+    {
+    	std::cout << "this is an exemple()\n";
+    }
+
+    // Here you create your pipelines and Descriptor Sets!
+	void pipelinesAndDescriptorSetsInit()
+	{
+		std::cout << "this is an exemple()\n";
+	}
+
+    // Here you destroy your pipelines and Descriptor Sets!
+	// All the object classes defined in Starter.hpp have a method .cleanup() for this purpose
+	void pipelinesAndDescriptorSetsCleanup()
+	{
+		std::cout << "this is an exemple()\n";
+	}
+
+    // Here you destroy all the Models, Texture and Desc. Set Layouts you created!
+	// All the object classes defined in Starter.hpp have a method .cleanup() for this purpose
+	// You also have to destroy the pipelines: since they need to be rebuilt, they have two methods: .cleanup() recreates them, while .destroy() delete them completely
+	void localCleanup()
+	{
+		std::cout << "this is an exemple()\n";
+	}
+
+    // Here it is the creation of the command buffer:
+	// You send to the GPU all the objects you want to draw, with their buffers and textures
+	void populateCommandBuffer(VkCommandBuffer commandBuffer, int currentImage)
+	{
+		std::cout << "this is an exemple()\n";
+	}
+
+    // Here is where you update the uniforms. Very likely this will be where you will be writing the logic of your application.
+	void updateUniformBuffer(uint32_t currentImage)
+	{
+		std::cout << "this is an exemple()\n";
+	}
 };
-
-void MonumentSimulator::setWindowParameters() {
-    std::cout << "setWindowParameters()\n";
-}
-
-void MonumentSimulator::onWindowResize(int w, int h) {
-    std::cout << "onWindowResize()\n";
-}
-
-void MonumentSimulator::localInit() {
-    std::cout << "localInit()\n";
-}
-
-void MonumentSimulator::pipelinesAndDescriptorSetsInit() {
-    std::cout << "pipelinesAndDescriptorSetsInit()\n";
-}
-
-void MonumentSimulator::populateCommandBuffer(VkCommandBuffer commandBuffer, int i) {
-    std::cout << "populateCommandBuffer()\n";
-}
-
-void MonumentSimulator::updateUniformBuffer(uint32_t currentImage) {
-    std::cout << "updateUniformBuffer()\n";
-}
-
-void MonumentSimulator::pipelinesAndDescriptorSetsCleanup() {
-    std::cout << "pipelinesAndDescriptorSetsCleanup()\n";
-}
-
-void MonumentSimulator::localCleanup() {
-    std::cout << "localCleanup()\n";
-}
-
+//-----------------------------------------------------------------------------------------------------
 //-----------------------------------------------------------------------------------------------------
 /*
  * MAIN FUNCTION: do not touch this
