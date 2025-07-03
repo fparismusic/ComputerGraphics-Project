d072c15e51efbea1883879bc89e9b9b614621798
fra-moretti <morettifra.23@gmail.com>
2025-07-03 18:36:58 +0200

    deleting starter change

--- MonumentSimulator/include/modules/Starter.hpp
+++ MonumentSimulator/include/modules/Starter.hpp
@@ -784,16 +784,7 @@ void BaseProject::initWindow() {
 
 	window = glfwCreateWindow(windowWidth, windowHeight, windowTitle.c_str(), nullptr, nullptr);
 
-	GLFWmonitor* monitor = glfwGetPrimaryMonitor();
-	const GLFWvidmode* mode = glfwGetVideoMode(monitor);
-	glfwSetWindowMonitor(
-		window,
-		monitor,
-		0, 0,
-		mode->width,
-		mode->height,
-		mode->refreshRate
-	);
+	
 
 	glfwSetWindowUserPointer(window, this);
 	glfwSetFramebufferSizeCallback(window, framebufferResizeCallback);
