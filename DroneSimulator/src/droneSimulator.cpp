02dd63348f6210eb4a38020a20b468655b92c792
Moein Zadeh <smmoeinp@gmail.com>
2025-07-07 12:23:44 +0200

    Cam pos update 2

--- DroneSimulator/src/droneSimulator.cpp
+++ DroneSimulator/src/droneSimulator.cpp
@@ -850,7 +850,6 @@ void DroneSimulator::updateUniformBuffer(uint32_t currentImage)
     // ─── Global UBO ────────────────────────────────────────────────────────────────────────────────────────────
     GUBO.proj       = proj;
     GUBO.view       = view;
-    GUBO.cameraPos  = CamPos = glm::vec3(glm::inverse(view)[3]);
     GUBO.lightDir   = glm::normalize(glm::vec3(0,1,0)); // Direction of the light source
     GUBO.time       = totalElapsedTime;
     updateGlobalUBO(GUBO, totalElapsedTime);
@@ -1015,7 +1014,6 @@ void DroneSimulator::reset()
 	gameOver = 0.0f;
 
 
-	CamPos = glm::vec3(0.0f, 0.3f, 2.0f);
 
 	thirdPerson   = true;
 	topAngle   = false;
